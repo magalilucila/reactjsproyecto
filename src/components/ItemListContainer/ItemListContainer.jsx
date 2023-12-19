976947f4f68aa986c5513dca1a69454e77e051d9
MagaliLucila <magalilucilarosa@gmail.com>
2023-12-19 20:43:32 -0300

    mi cuarto commit

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,28 +2,8 @@ import { useState, useEffect } from 'react'
 import { pedirDatos } from '../../utils/utils';
 import ItemList from '../itemlist/ItemList';
 import { useParams } from 'react-router-dom';
-/* const ItemlistContainer = () => {
-    return (
-        <section>
-        <div>
-            <h2 className='bg-rose-300 text-center text-3xl text-white'>Productos</h2>
-            <ul className="flex flex-col items-center md:flex-row md:justify-evenly flex-wrap">
-                <ItemList> Producto 1</ItemList>
-                <ItemList> Producto 2 </ItemList>
-                <ItemList> Producto 3 </ItemList>
-                <ItemList> Producto 4 </ItemList>
-                <ItemList> Producto 5 </ItemList>
-                <ItemList> Producto 6 </ItemList>
-                </ul>
-        </div>
-        </section>
-    )
-} 
-
-export default ItemListContainer
 
 
-*/
 const ItemListContainer = () => {
     const [productos, setProductos] = useState([]);
 
