6df6b97c30a4c6f86395687cb75178e47913570b
MagaliLucila <magalilucilarosa@gmail.com>
2024-01-19 23:07:47 -0300

    mi sexto commit

--- src/mock/upload.js
+++ src/mock/upload.js
@@ -1,30 +1,23 @@
-import { MOCK_DATA } from "./data.js" 
-
-
+import { MOCK_DATA}  from "./data.js" 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {addDoc, collection, doc, getFirestore} from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA0arj60KZ_b5rC5m-_ZHqXWQrimzW43cE",
-    authDomain: "reactjs2612.firebaseapp.com",
-    projectId: "reactjs2612",
-    storageBucket: "reactjs2612.appspot.com",
-    messagingSenderId: "638988161572",
-    appId: "1:638988161572:web:6cd2d6e11e86b10d4b3eb1",
-    measurementId: "G-K6FQ5LSGQV"
+    apiKey: "AIzaSyAqwg8TawQ-Igh4muvNYTHw-mrcCmlOUoU",
+    authDomain: "coder-proyecto1.firebaseapp.com",
+    projectId: "coder-proyecto1",
+    storageBucket: "coder-proyecto1.appspot.com",
+    messagingSenderId: "558853144456",
+    appId: "1:558853144456:web:239d5295d2923d3485c43d"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
-
-const productosRef = collection(db, 'productos',)
-MOCK_DATA.forEach(item => {
+const db = getFirestore (app)
+const productosRef = collection (db, 'Productos')
+MOCK_DATA.forEach(item =>{
     delete item.id
 
     addDoc(productosRef, item)
